cae8f026ba9809aab0138dede5d0a00b42b623f8
김용오 <138638694+GmumDev@users.noreply.github.com>
2026-08-06 13:43:48 +0900

    [솔버, 코드] 프리미티브 배치 유효성 검사기 구현
    - 솔버가 프리미티브를 플레이 영역 밖에 설치하지 못하게 하기 위함
    - 프리미티브 크기가 너무 작거나 크지 않도록 하기 위함
    - 잉크를 초과 사용하지 않도록 하기 위함

---
+++ Assets/_Project/Scripts/Solver/PlacementReject.cs
@@ -0,0 +1,25 @@
+namespace PPS.Solver
+{
+    /// <summary>
+    /// 프리미티브를 놓을 수 없는 이유.
+    /// 사유를 구분해야 후보 격자를 어느 축으로
+    /// 좁힐지 판단할 수 있다.
+    /// </summary>
+    public enum PlacementReject
+    {
+        /// 놓을 수 있다.
+        None = 0,
+
+        /// 공이 못 올라타거나 끼일 만큼 작다.
+        TooSmall = 1,
+
+        /// 혼자서 잉크 총량을 넘긴다.
+        TooLarge = 2,
+
+        /// 플레이 영역 밖이다.
+        OutOfArea = 3,
+
+        /// 이미 쓴 잉크와 합치면 총량을 넘긴다.
+        InkExceeded = 4,
+    }
+}
