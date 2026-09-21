337f6d9b0a958d9fda78a19202bc603965725f05
DongChan <266597618+muJiddo9q@users.noreply.github.com>
2026-09-21 13:23:50 +0900

    [도로롱툴, 코드] 장치 범위수정

--- Assets/_Project/Scripts/DrawingTool/Frontend/SimStageView.cs
+++ Assets/_Project/Scripts/DrawingTool/Frontend/SimStageView.cs
@@ -91,6 +91,7 @@ namespace PPS.DrawingTool
 
             _levelView.ResetBall();
             _levelView.ShowAll();
+            _levelView.ShowCountdown(0);
             HideFragments();
             StopKillEffect();
             _strokes.Rebuild();
@@ -109,6 +110,7 @@ namespace PPS.DrawingTool
             FollowPivots();
             FollowStars(world);
             FollowDevices(world);
+            _levelView.ShowCountdown(world.CurrentStep);
             FollowFragments(world);
             FollowKill(world);
         }
